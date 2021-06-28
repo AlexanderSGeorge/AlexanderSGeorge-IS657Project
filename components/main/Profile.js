c757ccf0cd50b7df02800a9c3c4a3ec7d660a65a
AlexanderSGeorge <Alex.George@student.csulb.edu>
2021-06-27 21:06:32 -0700

    4th Commit

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,11 +1,44 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { StyleSheet, View, Text, Image, FlatList, Button } from 'react-native'
+import firebase from 'firebase'
+require('firebase/firestore')
+import { connect } from 'react-redux'
 
-export default function Profile() {
+function Profile(props) {
+    const {currentUser, posts} = props;
+    console.log({currentUser, posts})
     return (
-        <View>
-            <Text> Profile </Text>
-            
+        <View style={styles.container}>
+            <View style={styles.containerInfo}>
+       
+            </View>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    containerInfo: {
+        margin: 20
+    },
+    containerGallery: {
+        flex: 1
+    },
+    containerImage: {
+        flex: 1 / 3
+
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1 / 1
+    }
+})
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    posts: store.userState.posts,
+    following: store.userState.following
+})
+export default connect(mapStateToProps, null)(Profile);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -6,6 +6,7 @@ import firebase from 'firebase'
 import { SnapshotViewIOSComponent } from 'react-native'
 require('firebase/firestore')
 
+
 export function fetchUser() {
     return ((dispatch) => {
         firebase.firestore()
@@ -14,12 +15,30 @@ export function fetchUser() {
             .get()
             .then((snapshot) => {
                 if (snapshot.exists) {
-                    console.log(snapshot)
                     dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() })
                 }
                 else {
                     console.log('does not exist')
                 }
+            })
+    })
+}
+
+export function fetchUserPosts() {
+    return ((dispatch) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data }
+                })
+                dispatch({ type: USER_POSTS_STATE_CHANGE, posts })
             })
     })
 }
